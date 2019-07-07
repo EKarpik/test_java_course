316d0217d7470119b48f2b63d14a9a6768420b3f
EKarpik <elena_karpik@gmail.com>
2019-07-07 21:05:30 +0300

    внесены функции для вычичлительных операций

--- sandbox/src/main/java/ru/stqu/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqu/pft/sandbox/MyFirstProgram.java
@@ -1,6 +1,23 @@
 package ru.stqu.pft.sandbox;
-public class MyFirstProgram{
-	public static void main (String[] args) {
-	System.out.println("Hello World!");
+public class MyFirstProgram {
+	public static void main(String[] args) {
+		hello("world!");
+		hello("Elena!");
+		hello("User!");
+
+	double len = 5;
+	System.out.println("Площадь квадрата со стороной"+len +"="+ area (len));
+
 	}
+
+	public static void hello (String somebody) {
+	System.out.println("Hello, " + somebody + "!");
+}
+
+
+	 public static double area (double l) {
+				return l*l;
+	}
+
+}
 }
