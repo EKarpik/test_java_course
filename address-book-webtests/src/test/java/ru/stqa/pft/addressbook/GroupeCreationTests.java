1ef3f3c938f75dfd2a24e05e20e4ae6b8f9a21f5
EKarpik <elena_karpik@gmail.com>
2019-08-21 12:06:28 +0300

    refactorng методов
    выполнены преобразования кода (вспомогательные методы, параметры и т.д.)

--- address-book-webtests/src/test/java/ru/stqa/pft/addressbook/GroupeCreationTests.java
+++ address-book-webtests/src/test/java/ru/stqa/pft/addressbook/GroupeCreationTests.java
@@ -1,15 +1,11 @@
 package ru.stqa.pft.addressbook;
 
-import java.util.regex.Pattern;
 import java.util.concurrent.TimeUnit;
 
-import org.junit.After;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.*;
-import static org.testng.Assert.*;
+
 import org.openqa.selenium.*;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.support.ui.Select;
 
 public class GroupeCreationTests {
   private WebDriver driver;
@@ -17,35 +13,59 @@ public class GroupeCreationTests {
 
   @BeforeMethod(alwaysRun = true)
   public void setUp() throws Exception {
-    System.setProperty( "webdriver.chrome.driver", "C:\\addressBook\\Drivers\\chromedriver.exe");
+    System.setProperty("webdriver.chrome.driver", "C:\\addressBook\\Drivers\\chromedriver.exe");
     driver = new ChromeDriver();
     driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-    driver.get("http://localhost/addressbook/");
-    driver.findElement(By.name("user")).click();
+    goToPage();
+    login("admin", "secret");
+  }
+
+  private void login(String username, String password) {
+    initGoupeCreation("user");
     driver.findElement(By.name("user")).clear();
-    driver.findElement(By.name("user")).sendKeys("admin");
-    driver.findElement(By.name("pass")).click();
+    driver.findElement(By.name("user")).sendKeys(username);
+    submitGroupeCreation("pass");
     driver.findElement(By.name("pass")).clear();
-    driver.findElement(By.name("pass")).sendKeys("secret");
+    driver.findElement(By.name("pass")).sendKeys(password);
     driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Пароль:'])[1]/following::input[2]")).click();
+  }
+
+  private void goToPage() {
+    driver.get("http://localhost/addressbook/");
   }
 
   @Test
   public void testGoupeCreation() throws Exception {
 
-    driver.findElement(By.linkText("Группы")).click();
-    driver.findElement(By.name("new")).click();
-    driver.findElement(By.name("group_name")).click();
+    goToGroupePage("Группы");
+    initGoupeCreation("new");
+    fillGroupeForm(new GroupeData("test1", "test2", "test3"));
+    submitGroupeCreation("submit");
+    goToGroupePage("group page");
+  }
+
+  private void submitGroupeCreation(String submit) {
+    driver.findElement(By.name(submit)).click();
+  }
+
+  private void fillGroupeForm(GroupeData groupeData) {
+    submitGroupeCreation("group_name");
     driver.findElement(By.name("group_name")).clear();
-    driver.findElement(By.name("group_name")).sendKeys("test1");
-    driver.findElement(By.name("group_header")).click();
+    driver.findElement(By.name("group_name")).sendKeys(groupeData.getName());
+    submitGroupeCreation("group_header");
     driver.findElement(By.name("group_header")).clear();
-    driver.findElement(By.name("group_header")).sendKeys("test2");
-    driver.findElement(By.name("group_footer")).click();
+    driver.findElement(By.name("group_header")).sendKeys(groupeData.getHeader());
+    submitGroupeCreation("group_footer");
     driver.findElement(By.name("group_footer")).clear();
-    driver.findElement(By.name("group_footer")).sendKeys("test3");
-    driver.findElement(By.name("submit")).click();
-    driver.findElement(By.linkText("group page")).click();
+    driver.findElement(By.name("group_footer")).sendKeys(groupeData.getFooter());
+  }
+
+  private void initGoupeCreation(String s) {
+    submitGroupeCreation(s);
+  }
+
+  private void goToGroupePage(String группы) {
+    driver.findElement(By.linkText(группы)).click();
   }
 
   @AfterMethod(alwaysRun = true)
