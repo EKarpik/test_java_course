9ee7af824c5d6761788f5b9b51eff49fd8484696
EKarpik <elena_karpik@gmail.com>
2019-09-08 23:29:21 +0300

    работа в  разных браузерах
    добавлены firefox, chrome,IE

--- address-book-webtests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ address-book-webtests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -2,7 +2,9 @@ package ru.stqa.pft.addressbook.appmanager;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.remote.BrowserType;
 import java.util.concurrent.TimeUnit;
 
 public class ApplicationManager {
@@ -12,10 +14,24 @@ public class ApplicationManager {
   private GroupeHelper groupeHelper;
   private SessionHelper sessionHelper;
 
+public ApplicationManager (String browser){
+  this.browser = browser;
+}
   public void init() {
-    System.setProperty("webdriver.chrome.driver", "C:\\addressBook\\Drivers\\chromedriver.exe");
-    driver = new ChromeDriver();
-    driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+    String browser = BrowserType.FIREFOX;
+    if (browser == BrowserType.FIREFOX) {
+        driver = new FirefoxDriver();
+    }
+      else if (browser == BrowserType.CHROME) {
+        driver = new ChromeDriver();
+      }
+      else if (browser == BrowserType.IE) {
+      driver = new InternetExplorerDriver();
+    }
+
+
+driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+  driver.get("C:\\addressBook\\Drivers\\chromedriver.exe");
     goToPage();
     groupeHelper = new GroupeHelper(driver);
     navigationHelper = new NavigationHelper(driver);
